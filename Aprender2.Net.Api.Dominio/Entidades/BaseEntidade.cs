69bd96b68aa66602c869687d6b951000c6ac2cc5
Fernando <fernandorobertoarx@gmail.com>
2021-06-28 00:07:45 -0300

    Criação dos novos projetos

---
+++ Aprender2.Net.Api.Dominio/Entidades/BaseEntidade.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Aprender2.Net.Api.Dominio.Entidades
+{
+    public abstract class BaseEntidade
+    {
+        [Key]
+        public int Id { get; set; }
+
+        private DateTime? dataCriado;
+
+        public DateTime? DataCriado { get => this.dataCriado; set { this.dataCriado = (value == null ? DateTime.UtcNow : value); } }
+
+        public DateTime? DataModificado { get; set; }
+    }
+}
---
+++ Aprender2.Net.Api.Dominio/Entidades/UsuarioEntidade.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aprender2.Net.Api.Dominio.Entidades
+{
+    public class UsuarioEntidade : BaseEntidade
+    {
+        public string Nome { get; set; }
+
+        public string Email { get; set; }
+    }
+}
